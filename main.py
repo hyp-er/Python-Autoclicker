16299218d66a82d9c85f7bacd87dc9d2f8745025
Hyperninja303 <62408125+Hyperninja303@users.noreply.github.com>
2021-05-04 16:17:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from pynput.mouse import Button, Controller
 mouse = Controller()
 
 for x in range(50):
-#clicks at 10.00 CPS (clicks per second)
+# clicks at 10.00 CPS 
     time.sleep(0.01)
     mouse.press(Button.left)
     mouse.release(Button.left)
 
-print('the autoclicker has run successfully!')
+print('the autoclicker has finished running!')
